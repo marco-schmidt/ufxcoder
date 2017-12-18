f28b4a41d87c05fb785583c78eb858f96cd5d532
Marco Schmidt <mschmidtgit@protonmail.com>
2017-12-18 20:34:56 +0100

    two jpeg tests: input too short (read error), invalid marker

---
+++ src/test/java/ufxcoder/formats/jpeg/JpegProcessorTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ufxcoder.formats.jpeg;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import org.junit.Assert;
+import org.junit.Test;
+import ufxcoder.app.AppConfig;
+
+/**
+ * Test {@link JpegProcessor} with various input streams.
+ */
+public class JpegProcessorTest
+{
+  private JpegProcessor create(final byte[] data)
+  {
+    final AppConfig config = new AppConfig();
+    config.setBundle(ResourceBundle.getBundle("Messages", Locale.ENGLISH));
+    config.setLocale(Locale.ENGLISH);
+    final JpegProcessor proc = new JpegProcessor();
+    proc.setConfig(config);
+    proc.open(data);
+    return proc;
+  }
+
+  @Test
+  public void testInputTooShort()
+  {
+    final JpegProcessor proc = create(new byte[]
+    {
+        0x17
+    });
+    proc.process();
+    final JpegFileDescription desc = proc.getJpegFileDescription();
+    Assert.assertTrue("Expect reading error because input is too short for even a two-byte marker. ",
+        desc.containsEvent(Msg.CANNOT_READ_HEADER));
+  }
+
+  @Test
+  public void testInvalidMarker()
+  {
+    final JpegProcessor proc = create(new byte[]
+    {
+        (byte) 0x9a, 0x44
+    });
+    proc.process();
+    final JpegFileDescription desc = proc.getJpegFileDescription();
+    Assert.assertTrue("Expect invalid marker error.", desc.containsEvent(Msg.INVALID_MARKER));
+  }
+}
