d2af405d4ca1aec93b286b9ef4a2685ef10c468d
Marco Schmidt <mschmidtgit@protonmail.com>
2019-03-30 23:47:03 +0100

    upgraded header with current year

--- src/main/java/ufxcoder/app/UniversalFileTranscoder.java
+++ src/main/java/ufxcoder/app/UniversalFileTranscoder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017, 2018 the original author or authors.
+ * Copyright 2017, 2018, 2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -144,7 +144,8 @@ public class UniversalFileTranscoder
 
     final PatternLayoutEncoder encoder = new PatternLayoutEncoder();
     encoder.setContext(loggerContext);
-    encoder.setPattern("%message%n");
+    // encoder.setPattern("%message%n");
+    encoder.setPattern(AppConfig.DEFAULT_LOGGING_PATTERN);
     encoder.start();
 
     final ConsoleAppender<ILoggingEvent> appender = new ConsoleAppender<ILoggingEvent>();
